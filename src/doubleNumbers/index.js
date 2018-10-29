8dca097f7da6bb8745638639741a5bccaf278dd8
myrosvas <myrosvas@gmail.com>
2018-10-29 21:17:14 +0200

    tasks done

--- src/doubleNumbers/index.js
+++ src/doubleNumbers/index.js
@@ -1,4 +1,7 @@
 /* eslint-disable-next-line */
 export const doubleNum = num => {
-  // your logic here...
+  const stringNum = num.toString();
+  const firstHalf = stringNum.substr(0, stringNum.length / 2);
+  const secondHalf = stringNum.substr(stringNum.length / 2, stringNum.length);
+  return firstHalf === secondHalf ? num : num * 2;
 };
--- src/sortByWeight/index.js
+++ src/sortByWeight/index.js
@@ -1,4 +1,11 @@
 /* eslint-disable-next-line */
-export const sortByWeight = arr => {
-  // your logic here...
+const getSum = strNum => {
+  const summedNum = strNum.split('').reduce((a, b) => Number(a) + Number(b), 0);
+  return summedNum;
 };
+
+
+export const sortByWeight = arr => {
+  return arr.sort((a, b) => getSum(a) - getSum(b));
+};
+
